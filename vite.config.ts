d5f27a3600e24e3b07c4091f15cd6aa4702b74c7
mostafaalpo4-hub <mostafaalpo4@gmail.com>
2026-02-17 23:08:56 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: './', // السطر ده هو اللي هيخلي GitHub Pages يشوف الملفات صح
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -22,4 +21,3 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
-
